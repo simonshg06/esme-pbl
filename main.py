4b9d095f1e786330a45efee6489f73d028799784
camy <camy.merdji@esme.fr>
2026-02-18 12:23:20 +0100

    Diagonal anti Diagonal

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ from tkinter import ttk
 class TicTacToe:
     def __init__(self,depth=9) #initialize the game and set the maximum depth
      self.max_depth = depth
+
+    def check_winner(self, board, player):
+    for i in range(3):
+        if all([board[i][j] == player for j in range(3)]): return True # Check rows
+        if all([board[j][i] == player for j in range(3)]): return True #Check clumns
+     if board[0][0] == board[1][1] == board[2][2] == player: return True #Check the main diagonal
+
+diagonal
+        if board[0][2] == board[1][1] == board[2][0] == player: return True
+anti-diagonal
+        return False # No winner
